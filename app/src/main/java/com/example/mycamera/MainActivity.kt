11c5809c0666107aedb717e1c063e10c66271865
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-01-18 14:53:35 +0900

    セットアップ処理

--- app/src/main/java/com/example/mycamera/MainActivity.kt
+++ app/src/main/java/com/example/mycamera/MainActivity.kt
@@ -2,12 +2,42 @@ package com.example.mycamera
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mycamera.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setupBinding()
+        setupRadioGroup()
+        setupButton()
     }
 
+    private fun setupBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun setupRadioGroup() {
+        binding.radioGroup.setOnCheckedChangeListener { radioGroup, checkedId ->
+            when(checkedId) {
+                R.id.preview -> binding.cameraButton.text = binding.preview.text
+                R.id.takePicture -> binding.cameraButton.text = binding.takePicture.text
+            }
+        }
+    }
+
+    private fun setupButton() {
+        binding.cameraButton.setOnClickListener {
+            when(binding.radioGroup.checkedRadioButtonId) {
+                R.id.preview -> preview()
+                R.id.takePicture -> takePicture()
+            }
+        }
+    }
+
+    private fun preview() { }
+    private fun takePicture() { }
 }
